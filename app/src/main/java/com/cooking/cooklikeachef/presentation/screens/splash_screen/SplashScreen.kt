93987710b9da3c6b7404fd86da567b99a87acd7d
stkokko <stelioskokko@yahoo.gr>
2022-05-01 15:59:57 +0300

    Creating SplashScreen

---
+++ app/src/main/java/com/cooking/cooklikeachef/presentation/screens/splash_screen/SplashScreen.kt
@@ -0,0 +1,48 @@
+package com.cooking.cooklikeachef.presentation.screens.splash_screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.airbnb.lottie.compose.*
+import com.cooking.cooklikeachef.R
+
+@Composable
+fun SplashScreen() {
+    val compositionSplash by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.splash))
+    val progressSplash by animateLottieCompositionAsState(
+        composition = compositionSplash,
+        iterations = LottieConstants.IterateForever
+    )
+    val compositionLoad by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.loading))
+    val progressLoad by animateLottieCompositionAsState(
+        composition = compositionLoad,
+        iterations = LottieConstants.IterateForever
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.general_color)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
+        Text(
+            text = stringResource(id = R.string.app_name),
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        LottieAnimation(composition = compositionSplash, progress = progressSplash)
+        LottieAnimation(composition = compositionLoad, progress = progressLoad)
+    }
+}
