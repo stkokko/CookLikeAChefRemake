f49534460340f577fc996e76fc2429eb215eae02
stkokko <stelioskokko@yahoo.gr>
2022-05-02 16:57:37 +0300

    Creating MainScreen

--- app/src/main/java/com/cooking/cooklikeachef/presentation/screens/main_screen/MainScreen.kt
+++ app/src/main/java/com/cooking/cooklikeachef/presentation/screens/main_screen/MainScreen.kt
@@ -1,10 +1,7 @@
 package com.cooking.cooklikeachef.presentation.screens.main_screen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +11,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.cooking.cooklikeachef.R
@@ -27,20 +26,32 @@ fun MainScreen(
         BottomNavigationBar(
             navController = navController
         )
-    }) {
-        Column(modifier = Modifier.fillMaxSize()) {
+    }) { innerPadding ->
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)) {
             Image(
                 painter = painterResource(id = R.drawable.home_image_background),
                 contentScale = ContentScale.Crop,
                 contentDescription = null
             )
         }
-        Box(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    start = 10.dp,
+                    top = 0.dp,
+                    end = 0.dp,
+                    bottom = innerPadding.calculateBottomPadding()
+                )
+        ) {
             Text(
                 text = stringResource(id = R.string.welcome_to_appname),
                 color = Color.White,
-                fontSize = 26.sp,
-                modifier = Modifier.align(alignment = Alignment.Center)
+                fontWeight = FontWeight.Bold,
+                fontSize = 28.sp,
+                modifier = Modifier.align(alignment = Alignment.CenterStart)
             )
             Column(
                 modifier = Modifier
@@ -49,6 +60,7 @@ fun MainScreen(
             ) {
                 Text(
                     text = stringResource(id = R.string.latest_recipes), color = Color.White,
+                    fontWeight = FontWeight.Bold,
                     fontSize = 22.sp
                 )
                 //TODO LazyRow
